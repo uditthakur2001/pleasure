83970990deb9347f43e848aeaa5b01333a9f143f
uditrajsingh <uditr.singh@decisionpoint.in>
2026-05-02 22:54:18 +0530

    logo

--- src/components/site/Logo.tsx
+++ src/components/site/Logo.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { Leaf } from "lucide-react";
 
 export const Logo = ({
   variant = "default",
@@ -17,11 +16,8 @@ export const Logo = ({
       className="group flex items-center gap-3"
       aria-label="Pleasure Pharmaceuticals home"
     >
-      {/* <span className="grid h-10 w-10 place-items-center rounded-md bg-primary text-primary-foreground shadow-soft transition-smooth group-hover:bg-primary-glow">
-        <Leaf className="h-5 w-5" />
-      </span> */}
       <span className="grid h-10 w-10 place-items-center rounded-md  text-primary-foreground shadow-soft transition-smooth group-hover:bg-primary-glow">
-        <img src="/pleasaure logo.svg" alt="logo" className="h-10 w-10" />
+        <img src="/public/pleasaure logo.svg" alt="logo" className="h-10 w-10" />
       </span>
       <span className="leading-tight">
         <span className={`block font-display text-lg ${text}`}>
